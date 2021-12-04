48fc286cf3169fe8299313b8bccfb0478012adb7
Phillip LeBlanc <phillip@spiceai.io>
2021-12-04 20:16:35 +0900

    revert back to normal yarn

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log("Hello")
+import puppeteer from 'puppeteer'
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://new.land.naver.com/complexes/12240?ms=37.539953,126.945308,17&a=APT:ABYG:JGC&e=RETAIL&articleNo=2132027944');
+  await page.screenshot({ path: 'naver_test.png' });
+
+  await browser.close();
+})();
